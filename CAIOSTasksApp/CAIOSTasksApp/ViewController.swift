4cacd62847aa165b486a510e0cd54ed9bd22402a
Rolandas-R <driveris@gmail.com>
2023-02-26 12:04:16 +0200

    View Controller corrections
    1. Pataisytos klaidos - pakeisti constraintai, apacioje vietoje klaidingai ideto textfieldo pakeista textlabeliu;
    2. Ikeltas virsuje verticalus StackViewsas ir i ji ideta icona bei textlabel su apps'o pavadinimu. Taip pat sutvarkyti constraintai;
    3. Sutvarkyti dizaino elementai kad kaip imanoma labiau sutaptu su uzduotimi;
    4. Pradinis navigacijos paspaudus mygtuka ir navigationalTextLabel'i igyvendinimas.

--- CAIOSTasksApp/CAIOSTasksApp/ViewController.swift
+++ CAIOSTasksApp/CAIOSTasksApp/ViewController.swift
@@ -13,17 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterUserNameField: UITextField!
     @IBOutlet weak var enterPasswordField: UITextField!
     @IBOutlet weak var enterCredentialsButton: UIButton!
-    @IBOutlet weak var navigationalTextLabel: UITextField!
-    
+    @IBOutlet weak var navigationalTextLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapFunction))
+                navigationalTextLabel.isUserInteractionEnabled = true
+                navigationalTextLabel.addGestureRecognizer(tap)
         // INIT
     }
 
     @IBAction func enterCredentialsButtonTapped(_ sender: Any) {
-        navigationalTextLabel.text = "You're tapped to register button"
+        navigationalTextLabel.text = "Already registered? Login!"
+    }
+    
+    @IBAction func tapFunction(sender: UITapGestureRecognizer) {
+        print("Label tapped")
+        enterCredentialsButton.titleLabel?.text = "Register"
+        navigationalTextLabel.text = "Have no username? Register!"
+        
     }
     
 }
