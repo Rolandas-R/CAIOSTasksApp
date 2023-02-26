14dbcaf804e17122cac1485193f0c9b0f1d6466e
Rolandas-R <driveris@gmail.com>
2023-02-26 09:46:25 +0200

    Init
    Pradzia 20223-02-26 10:00

---
+++ CAIOSTasksApp/CAIOSTasksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CAIOSTasksApp
+//
+//  Created by reromac on 2023-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // INIT
+    }
+
+
+}
+
