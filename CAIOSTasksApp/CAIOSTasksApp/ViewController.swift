c572586aea8c090669a08aa366338db0348505ef
Rolandas-R <driveris@gmail.com>
2023-02-26 10:59:58 +0200

    Outlets and test action for button; few changes in constraints

--- CAIOSTasksApp/CAIOSTasksApp/ViewController.swift
+++ CAIOSTasksApp/CAIOSTasksApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var enterUserNameField: UITextField!
+    @IBOutlet weak var enterPasswordField: UITextField!
+    @IBOutlet weak var enterCredentialsButton: UIButton!
+    @IBOutlet weak var navigationalTextLabel: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // INIT
     }
 
-
+    @IBAction func enterCredentialsButtonTapped(_ sender: Any) {
+        navigationalTextLabel.text = "You're tapped to register button"
+    }
+    
 }
 
