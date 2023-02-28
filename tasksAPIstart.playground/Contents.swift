d5a36edab9470d064af4d5b39cd0394c3a325bcd
Rolandas-R <driveris@gmail.com>
2023-02-28 21:46:03 +0200

    Created all basic methods work with swagerAPI
    Pagrindines f-jos kad butu galima:
    1. Register user;
    2. Login User;
    3. Delete User.
    
    O taip pat padaryti baziniai methodai, kad dirbti su taskais:
    1. Fetch all user tasks; (GET)
    2. Postinti nauja taska (POST);
    3. Gauti konkretu taska, pagal tasko ID (GET)
    4. Tasko redagavimas (PUT);
    5. Tasko trynimas (DELETE)

--- tasksAPIstart.playground/Contents.swift
+++ tasksAPIstart.playground/Contents.swift
@@ -14,13 +14,29 @@ struct UserDeleteRequest: Codable {
     let id: Int
 }
 
+struct TaskDeleteRequest: Codable {
+    let id: Int
+}
+
+struct Task: Codable {
+    let id: Int?
+    let title: String
+    let description: String
+    let estimateMinutes: Int
+    let assigneeId: Int?
+    var loggedTime: Int?
+    var isDone: Bool?
+}
+
+
 private var dataTask: URLSessionDataTask?
 
-let kUserId = 49
+let kUserId = 101
 
 
 let userTasksURL = "http://134.122.94.77/api/Task/userTasks"
 let userURL = "http://134.122.94.77/api/User"
+let taskURL =  URL(string:"http://134.122.94.77/api/Task")!
 
 
 
@@ -48,6 +64,7 @@ func buildDeleteUserURL(baseURL: String, id: Int) -> URL? {
     return URL(string: "\(baseURL)/\(id)")
 }
 
+
 func deleteRequest (url: URL, completion: @escaping (Data?) -> Void) {
     
     var request = URLRequest (url: url)
@@ -72,58 +89,65 @@ func buildQueryURL() -> URL? {
         let queryItems = [userIdQueryItem]
         components?.queryItems = queryItems
         guard let queryURL = components?.url else { return nil }
-        print(queryURL)
         return queryURL
     }
     return nil
 }
-
 buildQueryURL()
+
+func buildURLWithId(baseURL: URL?, id: Int) -> URL? {
+   let baseURL = taskURL
+   return URL(string: "\(baseURL)/\(id)")
+}
 
 
 // URL'ai:
-//let loginURL = URL(string: "http://134.122.94.77/api/user/login")!
-//let registerURL = URL(string: "http://134.122.94.77/api/User/register")!
-////let deleteUserURL = URL(string: "http://134.122.94.77/api/User/")!
-//let getUserTasksURL = URL(string: "http://134.122.94.77/api/Task/userTasks")
+let loginURL = URL(string: "http://134.122.94.77/api/user/login")!
+let registerURL = URL(string: "http://134.122.94.77/api/User/register")!
+let deleteUserURL = URL(string: "http://134.122.94.77/api/User/")!
+let newTaskURL = URL(string: "http://134.122.94.77/api/Task")!
+
 
 // MARK: - Requestai:
 
-// REGISTER USER
+// MARK: REGISTER USER
+/*
+let registerUserRequest = UserRegisterRequest(username: "123", password: "123")
+let registerData = try! JSONEncoder().encode (registerUserRequest)
 
-//let registerUserRequest = UserRegisterRequest(username: "DDDD", password: "DDDD")
-//let registerData = try! JSONEncoder().encode (registerUserRequest)
-//
-//postRequest(url: registerURL, body: registerData) { responseData in
-//        guard let responseData = responseData else { return }
-//        print (String (data: responseData, encoding: .utf8) ?? "nil")
-//}
+postRequest(url: registerURL, body: registerData) { responseData in
+        guard let responseData = responseData else { return }
+        print (String (data: responseData, encoding: .utf8) ?? "nil")
+}
+ */
 
-// LOGIN USER
+// MARK: LOGIN USER
 
-//let loginUserRequest = UserLoginRequest(username: "DDDD", password: "DDDD")
-//let loginData = try! JSONEncoder().encode (loginUserRequest)
-//
-//postRequest(url: loginURL, body: loginData) { responseData in
-//        guard let responseData = responseData else { return }
-//        print (String (data: responseData, encoding: .utf8) ?? "nil")
-//}
+let loginUserRequest = UserLoginRequest(username: "123", password: "123")
+let loginData = try! JSONEncoder().encode (loginUserRequest)
+
+postRequest(url: loginURL, body: loginData) { responseData in
+        guard let responseData = responseData else { return }
+        print (String (data: responseData, encoding: .utf8) ?? "nil")
+}
 
 
-// DELETE USER
+// MARK: DELETE USER
+/*
 
-//var deletingUser = UserDeleteRequest(id: 48)
-//let url = buildDeleteUserURL(baseURL: userURL, id: deletingUser.id)
-//deleteRequest(url: url!) { responseData in
-//    guard let responseData = responseData else { return }
-//    print (String (data: responseData, encoding: .utf8)!)
-//}
+var deletingUser = UserDeleteRequest(id: 49)
+let url = buildDeleteUserURL(baseURL: userURL, id: deletingUser.id)
+deleteRequest(url: url!) { responseData in
+    guard let responseData = responseData else { return }
+    print (String (data: responseData, encoding: .utf8)!)
+}
+*/
 
-// FETCH USER TASKS
-
+// MARK: FETCH ALL USER TASKS
+/*
 public func fetchUserTasks(completion: @escaping (Data?) -> Void) {
-    dataTask?.cancel()
-    
+//    dataTask?.cancel()
+
     if let queryURL = buildQueryURL() {
         dataTask = URLSession.shared.dataTask(with: queryURL) {
             data,
@@ -133,7 +157,6 @@ public func fetchUserTasks(completion: @escaping (Data?) -> Void) {
                 completion(nil)
                 return
             }
-            
             return completion(data)
         }
     }
@@ -142,13 +165,121 @@ public func fetchUserTasks(completion: @escaping (Data?) -> Void) {
 
 fetchUserTasks { data in
     DispatchQueue.main.async {
-        let result = try? JSONDecoder().decode(Data.self,
-                                               from: data!)
-        
+        let result = try? JSONDecoder().decode(Data.self, from: data!)
         print (String (data: data!, encoding: .utf8)!)
     }
 }
-            
+ */
+   
+
+// MARK: POST new task
+/*
+func postNewTask (url: URL, body: Data?, completion: @escaping (Data?) -> Void) {
+
+    var request = URLRequest (url: url)
+    request.httpMethod = "POST"
+    request.httpBody = body
+    request.addValue ("application/json", forHTTPHeaderField: "Content-Type")
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error ?? "Unknown error")
+            return
+        }
+        if let httpResponse = response as? HTTPURLResponse {
+            print("statusCode: \(httpResponse.statusCode)")
+        }
+        completion (data)
+        return
+    }.resume()
+}
+
+let postNewTaskRequest = Task(id: nil, title: "Boobs", description: "Fine", estimateMinutes: 10, assigneeId: kUserId)
+let taskData = try! JSONEncoder().encode (postNewTaskRequest)
+
+postNewTask(url: newTaskURL, body: taskData) { data in
+        guard let data = data else { return }
+    DispatchQueue.main.async {
+        let result = try? JSONDecoder().decode(Data.self, from: data)
+        print (String (data: data, encoding: .utf8)!)
+    }
+}
+*/
+
+// MARK: GET Task with id
+/*
+ func buildURLWithId(baseURL: URL?, id: Int) -> URL? {
+    let baseURL = taskURL
+    return URL(string: "\(baseURL)/\(id)")
+}
+
+func fetchTask(completion: @escaping (Data?) -> Void) {
+//    dataTask?.cancel()
+
+ if let queryURL = buildURLWithId(baseURL: taskURL, id: 111) {
+        dataTask = URLSession.shared.dataTask(with: queryURL) { data, response, error in
+            guard let data else {
+                completion(nil)
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse {
+                print("statusCode: \(httpResponse.statusCode)")
+                return completion(data)
+            }
+        }
+    }
+    dataTask?.resume()
+}
+
+fetchTask { data in
+    DispatchQueue.main.async {
+        let result = try? JSONDecoder().decode(Data.self, from: data!)
+        print (String (data: data!, encoding: .utf8)!)
+
+    }
+}
+ */
+
+
+// MARK: EDIT TASK (PUT):
+/*
+func editTask(url: URL, body: Data?, completion: @escaping (Data?) -> Void) {
+    var request = URLRequest (url: taskURL)
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpMethod = "PUT"
+    request.httpBody = body
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+      guard let data = data else {
+        print(String(describing: error))
+        return
+      }
+      print(String(data: data, encoding: .utf8)!)
+    }
+    task.resume()
+}
+
+let editTaskRequest = Task(id: 111, title: "UzduotisRaaa2", description: "aprasymasaaa2", estimateMinutes: 10, assigneeId: 101, loggedTime: 10, isDone: true)
+let editTaskData = try! JSONEncoder().encode (editTaskRequest)
+
+editTask(url: taskURL, body: editTaskData) { data in
+        guard let data = data else { return }
+    DispatchQueue.main.async {
+        let result = try? JSONDecoder().decode(Data.self, from: data)
+        print (String (data: data, encoding: .utf8)!)
+    }
+}
+ */
+
+
+// MARK: DELETE TASK
+/*
+var deletingtask = TaskDeleteRequest(id: 111)
+let url = buildURLWithId(baseURL: taskURL, id: deletingtask.id)
+deleteRequest(url: url!) { responseData in
+    guard let responseData = responseData else { return }
+    print (String (data: responseData, encoding: .utf8)!)
+}
+ */
+
 
             
 
